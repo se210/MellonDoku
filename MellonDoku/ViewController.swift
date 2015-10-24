936b3437dd126d04e0475426acd934d183e8e518
Gihyuk Ko <fishhyuk11@gmail.com>
2015-10-23 22:11:45 -0400

    Initial Commit

---
+++ MellonDoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MellonDoku
+//
+//  Created by Gihyuk Ko on 10/23/15.
+//  Copyright © 2015 Gihyuk Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
