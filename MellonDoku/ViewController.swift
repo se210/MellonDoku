7f57af2104b4b9f51b9a7c659bb4663af4ae18e9
Gihyuk Ko <fishhyuk11@gmail.com>
2015-10-27 19:19:22 -0400

    Added details for sudoku
     - generated buttons for sudoku
     - filled in printsudoku method

--- MellonDoku/ViewController.swift
+++ MellonDoku/ViewController.swift
@@ -89,20 +89,25 @@ class GameSetupViewController: UIViewController {
 class GameViewController: GameSetupViewController {
     
     var sudoku: SudokuGen!
+    var numbers: [UIButton] = [UIButton(type: UIButtonType.Custom) as UIButton]
     
     override func viewDidLoad() {
+        
         // draw puzzle frame
+        
+        let navibar = self.navigationController!.navigationBar.frame.size.height
         let framepath = CGPathCreateMutable()
         let puzzleframe = CAShapeLayer()
         
+        let puzzlesize = CGRectGetMaxX(screenBound) * 0.9
         let initial = (x: CGRectGetMaxX(screenBound) * 0.05,
-                       y: (CGRectGetMaxY(screenBound) - CGRectGetMaxX(screenBound)) * 0.6)
+                       y: 0.5 * navibar + (CGRectGetMaxY(screenBound) - puzzlesize) * 0.5)
         let final = (x: CGRectGetMaxX(screenBound) * 0.95,
-                     y: (CGRectGetMaxY(screenBound) - CGRectGetMaxX(screenBound)) * 0.6 + CGRectGetMaxX(screenBound) * 0.9)
+                     y: 0.5 * navibar + (CGRectGetMaxY(screenBound) - puzzlesize) * 0.5 + puzzlesize)
         let divide1 = (x: CGRectGetMaxX(screenBound) * 0.35,
-                       y: (CGRectGetMaxY(screenBound) - CGRectGetMaxX(screenBound))*0.6 + CGRectGetMaxX(screenBound) * 0.3)
+                       y: 0.5 * navibar + (CGRectGetMaxY(screenBound) - puzzlesize) * 0.5 + puzzlesize/3)
         let divide2 = (x: CGRectGetMaxX(screenBound) * 0.65,
-                       y: (CGRectGetMaxY(screenBound) - CGRectGetMaxX(screenBound)) * 0.6 + CGRectGetMaxX(screenBound) * 0.6)
+                       y: 0.5 * navibar + (CGRectGetMaxY(screenBound) - puzzlesize) * 0.5 + 2 * puzzlesize/3)
         
         CGPathMoveToPoint(framepath, nil, initial.x, initial.y)
         CGPathAddLineToPoint(framepath, nil, final.x, initial.y)
@@ -117,6 +122,31 @@ class GameViewController: GameSetupViewController {
         CGPathAddLineToPoint(framepath, nil, divide2.x, final.y)
         CGPathMoveToPoint(framepath, nil, initial.x, divide2.y)
         CGPathAddLineToPoint(framepath, nil, final.x, divide2.y)
+        
+        let buttonmargin: CGFloat = 3.0
+        let buttonsize = puzzlesize / 9 - 2 * buttonmargin
+        /*
+        let button = UIButton(type: UIButtonType.Custom) as UIButton
+        button.frame = CGRectMake(initial.x + buttonmargin, initial.y + buttonmargin, buttonsize, buttonsize)
+        button.setBackgroundImage(UIImage(named: "melon1"), forState: UIControlState.Normal)
+        button.addTarget(self, action: "userInput", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(button)*/
+        
+        // generate buttons for the sudoku
+        for i in 0...8 {
+            for j in 0...8 {
+                if (i != 0 || j != 0) {
+                    numbers.append(UIButton(type: UIButtonType.Custom) as UIButton)
+                }
+                numbers[9*i+j].frame = CGRectMake(initial.x + buttonmargin + CGFloat(i) * (buttonsize + 2 * buttonmargin),
+                                                  initial.y + buttonmargin + CGFloat(j) * (buttonsize + 2 * buttonmargin),
+                                                  buttonsize, buttonsize)
+                //numbers[9*i+j].backgroundColor = UIColor.greenColor()
+                //numbers[9*i+j].addTarget(self, action: "userInput", forControlEvents: UIControlEvents.TouchUpInside)
+                self.view.addSubview(numbers[9*i+j])
+            }
+        }
         
         puzzleframe.path = framepath
         puzzleframe.lineWidth = 2.0
@@ -145,13 +175,24 @@ class GameViewController: GameSetupViewController {
     
     func printSudoku(puzzle: [[Int]]) {
         // todo
-        // print sudoku
+        for i in 0...8 {
+            for j in 0...8 {
+                if (puzzle[i][j] != 0) {
+                    let image: String = "melon" + String(puzzle[i][j])
+                    numbers[9*i+j].setBackgroundImage(UIImage(named: image), forState: UIControlState.Normal)
+                    numbers[9*i+j].backgroundColor = UIColor.grayColor()
+                }
+                else {
+                    numbers[9*i+j].addTarget(self, action: "userInput", forControlEvents: UIControlEvents.TouchUpInside)
+                }
+            }
+        }
     }
     
     func checkSolved(puzzle: [[Int]]) {
         // todo
         if (puzzle == sudoku.solution) {
-            // congratulations!
+            // print congratulations!
         }
     }
     
