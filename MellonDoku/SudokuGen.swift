210bee95e32c332522a3b890d2e6606639a1455e
Gihyuk Ko <fishhyuk11@gmail.com>
2015-11-01 16:45:16 -0500

    Randomizing algorithm
     - added randomizing algorithm for the sudoku generation

--- MellonDoku/SudokuGen.swift
+++ MellonDoku/SudokuGen.swift
@@ -20,8 +20,7 @@ class SudokuGen {
     }
     
     func genSudoku(diff: String) -> (puzzle: [[Int]], solution:[[Int]]) {
-        // todo
-        let easyPuzzle: [[Int]] = [[3,0,1,5,8,6,7,4,9],
+        var easyPuzzle: [[Int]] = [[3,0,1,5,8,6,7,4,9],
                                    [9,7,5,3,4,0,1,0,6],
                                    [6,0,8,0,7,0,5,2,0],
                                    [0,1,0,4,5,7,9,6,2],
@@ -30,7 +29,7 @@ class SudokuGen {
                                    [0,8,4,0,9,0,2,0,7],
                                    [2,0,6,0,3,8,4,9,1],
                                    [7,3,9,1,2,4,6,0,8]]
-        let mediumPuzzle: [[Int]] = [[0,0,0,0,0,8,1,4,0],
+        var mediumPuzzle: [[Int]] = [[0,0,0,0,0,8,1,4,0],
                                      [0,0,0,0,0,0,9,2,0],
                                      [5,4,9,0,0,0,0,0,0],
                                      [4,0,0,0,8,7,0,0,0],
@@ -39,7 +38,7 @@ class SudokuGen {
                                      [0,0,0,0,0,0,2,8,6],
                                      [0,7,1,0,0,0,0,0,0],
                                      [0,5,2,4,0,0,0,0,0]]
-        let hardPuzzle: [[Int]] = [[0,0,0,0,0,0,0,6,8],
+        var hardPuzzle: [[Int]] = [[0,0,0,0,0,0,0,6,8],
                                    [0,9,5,0,0,6,7,0,2],
                                    [0,0,0,0,0,7,0,0,0],
                                    [0,0,0,0,4,5,3,0,0],
@@ -48,23 +47,25 @@ class SudokuGen {
                                    [0,0,0,5,0,0,0,0,0],
                                    [4,0,9,3,0,0,8,5,0],
                                    [5,2,0,0,0,0,0,0,0]]
-        var easySolution: [[Int]] = easyPuzzle
-        var mediumSolution: [[Int]] = mediumPuzzle
-        var hardSolution: [[Int]] = hardPuzzle
-        
-        solveSudoku(&easySolution)
-        solveSudoku(&mediumSolution)
-        solveSudoku(&hardSolution)
 
         switch (diff) {
         case "Easy":
+            randomizePuzzle(&easyPuzzle)
+            var easySolution: [[Int]] = easyPuzzle
+            solveSudoku(&easySolution)
             return (easyPuzzle, easySolution)
         case "Medium":
+            randomizePuzzle(&mediumPuzzle)
+            var mediumSolution: [[Int]] = mediumPuzzle
+            solveSudoku(&mediumSolution)
             return (mediumPuzzle, mediumSolution)
         case "Hard":
+            randomizePuzzle(&hardPuzzle)
+            var hardSolution: [[Int]] = hardPuzzle
+            solveSudoku(&hardSolution)
             return (hardPuzzle, hardSolution)
         default:
-            return (easyPuzzle, easySolution)
+            return ([[0]],[[0]])
         }
     }
     
@@ -99,8 +100,8 @@ class SudokuGen {
         }
         
         return checkArray(box, index: boxindex) &&
-            checkArray(row, index: position.1) &&
-            checkArray(column, index: position.0)
+               checkArray(row, index: position.1) &&
+               checkArray(column, index: position.0)
     }
     
     func findUnassignedLocation(puzzle: [[Int]], inout row: Int, inout col: Int) -> Bool {
@@ -135,25 +136,84 @@ class SudokuGen {
         return false
     }
     
-    /*func makeSudoku(puzzle: [[Int]]) -> [[Int]] {
-        var nextpuzzle: [[Int]] = puzzle
-        for i in 0...8 {
-            for j in 0...8 {
-                nextpuzzle = backTrack(nextpuzzle, position: (j,i))
+    func randomizePuzzle(inout puzzle: [[Int]]) {
+        let rowperm: [Int] = [Int(arc4random_uniform(6)), Int(arc4random_uniform(6)), Int(arc4random_uniform(6))]
+        let colperm: [Int] = [Int(arc4random_uniform(6)), Int(arc4random_uniform(6)), Int(arc4random_uniform(6))]
+        let symcount: Int = Int(arc4random_uniform(4))
+        var symperm: [(Int,Int)] = []
+        for i in 0...symcount {
+            symperm.append((Int(arc4random_uniform(9)) + 1, Int(arc4random_uniform(9)) + 1))
+        }
+        for i in 0...2 {
+            switch (rowperm[i]) {
+            case 1:
+                permuteRow(&puzzle, rows: (3*i+1,3*i+2))
+            case 2:
+                permuteRow(&puzzle, rows: (3*i+0,3*i+1))
+            case 3:
+                permuteRow(&puzzle, rows: (3*i+0,3*i+1))
+                permuteRow(&puzzle, rows: (3*i+0,3*i+2))
+            case 4:
+                permuteRow(&puzzle, rows: (3*i+0,3*i+1))
+                permuteRow(&puzzle, rows: (3*i+1,3*i+2))
+            case 5:
+                permuteRow(&puzzle, rows: (3*i+0,3*i+2))
+            default:
+                break
+            }
+            switch (colperm[i]) {
+            case 1:
+                permuteColumn(&puzzle, columns: (3*i+1,3*i+2))
+            case 2:
+                permuteColumn(&puzzle, columns: (3*i+0,3*i+1))
+            case 3:
+                permuteColumn(&puzzle, columns: (3*i+0,3*i+1))
+                permuteColumn(&puzzle, columns: (3*i+0,3*i+2))
+            case 4:
+                permuteColumn(&puzzle, columns: (3*i+0,3*i+1))
+                permuteColumn(&puzzle, columns: (3*i+1,3*i+2))
+            case 5:
+                permuteColumn(&puzzle, columns: (3*i+0,3*i+2))
+            default:
+                break
             }
         }
-        return nextpuzzle
-    }*/
+        for item in symperm {
+            permuteSymbol(&puzzle, numbers: item)
+        }
+    }
     
-    /*
-    //print(makeSudoku(sudoku.puzzle))
-    func solveSudoku(puzzle: [[Int]]) -> [[Int]] {
-        var nextpuzzle: [[Int]] = puzzle
+    func permuteRow(inout puzzle: [[Int]], rows: (Int,Int)) {
+        let temp: [Int] = puzzle[rows.0]
+        puzzle[rows.0] = puzzle[rows.1]
+        puzzle[rows.1] = temp
+    }
+    
+    func permuteColumn(inout puzzle: [[Int]], columns: (Int,Int)) {
+        var temp: [Int] = []
+        for i in 0...8 {
+            temp.append(puzzle[i][columns.0])
+            puzzle[i][columns.0] = puzzle[i][columns.1]
+        }
+        for i in 0...8 {
+            puzzle[i][columns.1] = temp[i]
+        }
+    }
+    
+    func permuteSymbol(inout puzzle: [[Int]], numbers: (Int,Int)) {
+        var index: [(Int,Int)] = []
         for i in 0...8 {
             for j in 0...8 {
-                nextpuzzle = backTrack(nextpuzzle, position: (j,i))
+                if (puzzle[i][j] == numbers.0) {
+                    index.append((i,j))
+                }
+                if (puzzle[i][j] == numbers.1) {
+                    puzzle[i][j] = numbers.0
+                }
             }
         }
-        return nextpuzzle
-    }*/
+        for item in index {
+            puzzle[item.0][item.1] = numbers.1
+        }
+    }
 }
